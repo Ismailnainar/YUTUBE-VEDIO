4cb16a158f543c6c9537b56822c2ba26b3b2d471
ismailnainar <ismailmohamedismail124@gmail.com>
2026-02-24 10:29:51 +0530

    Automated merge via Dashboard from main

--- API_TEST_APP/models.py
+++ API_TEST_APP/models.py
@@ -6,3 +6,13 @@ class Token(models.Model):
 
     def __str__(self):
         return self.token
+
+# from django.db import models
+
+# class Token(models.Model):
+#     token = models.CharField(max_length=20, unique=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.token
+

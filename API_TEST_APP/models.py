0db6e6f0669819e45e941a339889664d46ec1cb7
Ismail <111350275+Ismailnainar@users.noreply.github.com>
2026-02-25 16:37:07 +0530

    Clean up models.py by removing commented code
    Removed commented-out code for ShipmentID_Models class.

--- API_TEST_APP/models.py
+++ API_TEST_APP/models.py
@@ -7,58 +7,5 @@ class Token(models.Model):
     def __str__(self):
         return self.token
 
-# from django.db import models, transaction, IntegrityError
-# from django.utils import timezone
 
-
-# class ShipmentID_Models(models.Model):
-#     shipment_id = models.CharField(max_length=20, unique=True)
-#     tocken = models.PositiveIntegerField(default=0)
-
-#     class Meta:
-#         db_table = "WHR_UNIQUE_SHIPMENT_ID"
-#         ordering = ["id"]
-
-#     @staticmethod
-#     def generate_shipment_id():
-#         today = timezone.now()
-#         year = today.strftime("%y")   # last 2 digits of year
-#         month = today.strftime("%m")  # 2-digit month
-
-#         prefix = f"INO{year}{month}"
-
-#         # Only check current month's records
-#         last_record = (
-#             ShipmentID_Models.objects
-#             .filter(shipment_id__startswith=prefix)   # filter by this month's prefix
-#             .select_for_update(skip_locked=True)
-#             .order_by("-tocken")
-#             .first()
-#         )
-
-#         if last_record:
-#             next_tocken = last_record.tocken + 1
-#         else:
-#             next_tocken = 1
-
-#         tocken_str = str(next_tocken).zfill(2)  # 01, 02, 03...
-#         shipment_code = f"{prefix}{tocken_str}"
-#         return shipment_code, next_tocken
-
-#     def save(self, *args, **kwargs):
-#         if not self.shipment_id:
-#             for _ in range(600):  # retry loop
-#                 try:
-#                     with transaction.atomic():
-#                         self.shipment_id, self.tocken = self.generate_shipment_id()
-#                         super().save(*args, **kwargs)
-#                         return
-#                 except IntegrityError:
-#                     continue
-#             raise IntegrityError("Failed to generate unique Shipment ID after retries.")
-#         else:
-#             super().save(*args, **kwargs)
-
-#     def __str__(self):
-#         return self.shipment_id
 
